604b8ccd90131afeef65b786266cafbd48f73185
NeilBrooks <neil.brooks87@gmail.com>
2023-04-07 19:59:48 +0100

    Kata and tests written. Not on slack yet - having an ESM error.

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+
 /* 
 👉 Write your kata here!
+👉 Title: Defeat the enemy!
+👉 Description: Your simple adventure game is off to a flying start, but now you need to add a combat system.
+// Your function will take three inputs - firstly the player's attack agility, secondly their attack, and finally the enemy's health.
+// First, give the player an agility check. The idea is that the player's agility will be a number between 1 and 10.
+// If the player's agility is greater than 5, they will be able to attack the enemy. If it's higher than ten, nothing will happen, because they overpowered themselves - they're too agile!
+// If the player's agility check is successful, subtract the player's attack from the enemy's health. Both the player's attack and the enemy's health can be anything you like.
+// Make it so that the enemy's health is 0 or less, the enemy is defeated.
+
+// We want a very specific output from this function, so feel free to copy the below:
+
+- If the player's agility check is successful and they make a non-lethal hit, return (`You hit the enemy! Their health is now ${enemyHealth}!`)
+- If they make a lethal hit, return (`You hit the enemy! They are defeated!`)
+- If the player's agility check is unsuccessful, return (`You missed!`)
+- If the player's agility check is overpowered, return (`Not this time - you overpowered yourself!`)
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function attack(playerAgility, playerAttack, enemyHealth) {
+  // Write your code here!
+  return output;
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,48 @@
+const { test, expect } = require("@jest/globals")
+
 //👉 Write your tests below here:
+
+// Note: I'm currently having an import issue with the main.js file. I've got some issue with my ESM - and I've tried everything.
+// I'll get some help on this next week, but for now, I'm just going to write a shell of the function below so you can run your tests.
+// I'll ask for help on this next week but if you spot whatever I've done to mess it up, you'll be my best friend for life if you drop me a message on Slack.
+
+function attack (playerAgility, playerAttack, enemyHealth) {
+    if (playerAgility >=5 && playerAgility <= 10) {
+        enemyHealth = enemyHealth - playerAttack
+        output = (`You hit the enemy! Their health is now ${enemyHealth}!`)
+        if (enemyHealth <= 0) {
+            output = (`You hit the enemy! They are defeated!`)
+        }
+    }
+    else if (playerAgility < 5) {
+        output = (`You missed!`)
+    }
+    else if (playerAgility > 10) {
+        output = (`Not this time - you overpowered yourself!`)
+    }
+    return output;
+  }
+  
+test("Successful hit, no defeat", ()=> {
+    const actual = attack(5, 10, 50)
+    const expected = "You hit the enemy! Their health is now 40!"
+    expect(actual).toBe(expected)
+});
+
+test("Successful hit, defeat", ()=> {
+    const actual = attack(5, 10, 10)
+    const expected = "You hit the enemy! They are defeated!"
+    expect(actual).toBe(expected)
+});
+
+test("Unsuccessful hit", ()=> {
+    const actual = attack(3, 10, 50)
+    const expected = "You missed!"
+    expect(actual).toBe(expected)
+});
+
+test("Overpowered hit", ()=> {
+    const actual = attack(11, 10, 50)
+    const expected = "Not this time - you overpowered yourself!"
+    expect(actual).toBe(expected)
+});
